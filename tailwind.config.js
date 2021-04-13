28ceae7d196b5a2de57def59afcf750d512ca035
Fabian <fabian@digitalartdesigners.com>
2021-04-13 18:28:31 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        colors: {
+            'darkblue': '#24283b',
+            'purple': '#bb9af7',
+            'cyan': '#2ac3de',
+            'green': '#9ece6a',
+            'red': '#f7768e',
+            'white': '#c0caf5',
+        }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
